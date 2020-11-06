8cb02f4ee663e8747f890faf34e7ab8beb28dd80
Mario Mendonca <mariomeendonca00@gmail.com>
2020-11-06 17:52:35 -0300

    Começando a usar branchs diferentes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,41 @@ export default function Home() {
     <Header />
     <div className='container'>
       <div className='filter-container'>
+
+        <label>Cidade: </label>
+        <select>
+          <option>Recife</option>
+          <option>Rio de Janeiro</option>
+        </select>
+
+        <label>Bairro: </label>
+        <select>
+          <option>Aflitos</option>
+          <option>Espinheiro</option>
+        </select>
         
+        <label>Tamanho (m²): </label>
+        <select>
+          <option>70m2- 120m2</option>
+          <option>120 - 150</option>
+          <option>150 - 200</option>
+        </select>
+
+        <label>Preço: </label>
+        <select>
+          <option>1000 - 1200</option>
+          <option>1200- 1500</option>
+          <option>1500 - 2000</option>
+          <option>2000 -</option>
+        </select>
+
+        <label>Quartos: </label>
+        <select>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+        </select>
       </div>
       <div className='imoveis-container'>
         <ul>
