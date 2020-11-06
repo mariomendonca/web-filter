97c8312f8878a29fa51a296e96cbdf4b4b34235a
Andre <andreluismoura22@gmail.com>
2020-11-05 22:43:13 -0300

    Versionando maroto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,6 @@ import '../styles/home.css'
 import { Link } from 'react-router-dom'
 
 export default function Home() {
-  console.log(data)
 
   return (
     <>
@@ -18,20 +17,20 @@ export default function Home() {
       <div className='imoveis-container'>
         <ul>
           {data.map(produto => (
-            <li key={produto.id}>
-              <Link to='/a'> 
-              <strong>Cidade: </strong>
-              <p>{produto.cidade}</p>
-              <strong>Bairro:</strong>
-              <p>{produto.bairro}</p>
-              <strong>Tamanho: </strong>
-              <p>{produto.tamanho}</p>
-              <strong>Preço: </strong>
-              <p>{produto.preco}</p>
-              <strong>Quartos: </strong>
-              <p>{produto.quartos}</p>
-              </Link>
-            </li>
+            <Link to={`/${produto.id}`}> 
+              <li key={produto.id}>
+                <strong>Cidade: </strong>
+                <p>{produto.cidade}</p>
+                <strong>Bairro:</strong>
+                <p>{produto.bairro}</p>
+                <strong>Tamanho: </strong>
+                <p>{produto.tamanho}</p>
+                <strong>Preço: </strong>
+                <p>{produto.preco}</p>
+                <strong>Quartos: </strong>
+                <p>{produto.quartos}</p>
+              </li>
+            </Link>
           ))}
         </ul>
       </div>
