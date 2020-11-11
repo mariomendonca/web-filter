6c10dd6a817057aad68d6b1b613cf6c536ad7264
Mario Mendonca <mariomeendonca00@gmail.com>
2020-11-11 12:56:30 -0300

    iniciando pagina de ver mais e mapa

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
+import { useParams, Link } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
+// import { MapContainer, TileLayer } from 'react-leaflet'
+// import 'leaflet/dist/leaflet.css'
 
 import Header from '../components/Header'
-// import data from '../data/data'
+import data from '../data/data'
+import '../styles/product.css'
+
 
 export default function Product() {
+  const { id } = useParams()
+  const imovel = data.filter(produto => produto.id == id)
+
   return (
     <>
+      {console.log(imovel, id)}
       <Header />
-      <h2>produtos</h2>
+      <div className="produto-container">
+        <h2>test {imovel[0].cidade}</h2>
+        <Link to='/'>
+          <FiArrowLeft/> Voltar
+        </Link>
+
+        {/* <MapContainer
+          center={[-8.0504581, -34.8952614]}
+          zoom={15}
+          style={{ width: '100%', height: '100% '}}
+        >
+          <TileLayer 
+            url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+          />
+        </MapContainer> */}
+      </div>
     </>
   )
 }
