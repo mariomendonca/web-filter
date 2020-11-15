2fa4fafd57595480f459de1546ec1c96e2521638
Andre <andreluismoura22@gmail.com>
2020-11-15 15:50:24 -0300

    CSS nos botoes de paginção

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
+import '../styles/pagination.css'
+
 export default function Pagination({ cardsPerPage, totalCards, paginate }) {
   const pageNumbers = []
 
